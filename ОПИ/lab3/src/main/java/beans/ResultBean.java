edb56b7d5e299f96d9a9bd06cbe1769af5ce9147
andrey <ad061333@gmail.com>
2024-06-01 23:42:07 +0300

    правки opi_lab3

--- ОПИ/lab3/src/main/java/beans/ResultBean.java
+++ ОПИ/lab3/src/main/java/beans/ResultBean.java
@@ -15,67 +15,65 @@ import java.time.format.DateTimeFormatter;
 @ToString
 public class ResultBean implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(nullable = false) @Setter @Getter
-	private @NotNull Double x;
-	@Column(nullable = false) @Setter @Getter
-	private @NotNull Double y;
-	@Column(nullable = false) @Setter @Getter
-	private @NotNull Double r;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    @Setter
+    @Getter
+    private @NotNull Double x;
+    @Column(nullable = false)
+    @Setter
+    @Getter
+    private @NotNull Double y;
+    @Column(nullable = false)
+    @Setter
+    @Getter
+    private @NotNull Double r;
 
-	@Column(nullable = false) @Setter @Getter
-	private @NotNull String time;
+    @Column(nullable = false)
+    @Setter
+    @Getter
+    private @NotNull String time;
 
-	@Column(nullable = false) @Setter @Getter
-	private @NotNull String executionTime;
+    @Column(nullable = false)
+    @Setter
+    @Getter
+    private @NotNull String executionTime;
 
-	@Column(nullable = false) @Setter
-	private @NotNull boolean isHit;
+    @Column(nullable = false)
+    @Setter
+    private @NotNull boolean isHit;
 
-	public ResultBean() {}
+    public ResultBean() {
+    }
 
-	public ResultBean(ResultBean result) {
-		this.x = result.x;
-		this.y = result.y;
-		this.r = result.r;
-	}
+    public ResultBean(ResultBean result) {
+        this.x = result.x;
+        this.y = result.y;
+        this.r = result.r;
+    }
 
-	public boolean getIsHit() {
-		return isHit;
-	}
+    public boolean getIsHit() {
+        return isHit;
+    }
 
-	@PrePersist
-	protected void prePersist() {
-		this.time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-		this.isHit = checkHit();
-		this.setExecutionTime(String.valueOf( 0 + Math.random() * (0.3 - 0 + 0.1) ));
-	}
+    @PrePersist
+    protected void prePersist() {
+        this.time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        this.isHit = checkHit();
+        this.setExecutionTime(String.valueOf(0 + Math.random() * (0.3 - 0 + 0.1)));
+    }
 
-	public void setExecutionTime(String executionTime) {
-		this.executionTime = executionTime;
-	}
+    public void setExecutionTime(String executionTime) {
+        this.executionTime = executionTime;
+    }
 
-	private boolean checkHit() {
-		boolean area1_hit = (x >= 0 && y <= 0 && (r / 2) >= (x * x + y * y));
-		boolean area2_hit = (x >= 0 && x <= 2 * r && y >= 0 && y <= r / 2);
-		boolean area3_hit = (x <= 0 && y >= 0 && y <= x + r / 2);
+    private boolean checkHit() {
+        boolean area1_hit = (x >= 0 && y <= 0 && (r / 2) >= (x * x + y * y));
+        boolean area2_hit = (x >= 0 && x <= 2 * r && y >= 0 && y <= r / 2);
+        boolean area3_hit = (x <= 0 && y >= 0 && y <= x + r / 2);
 
-		return area1_hit || area2_hit || area3_hit;
-	}
-
-	public void setHit(boolean hit) {
-		isHit = hit;
-	}
-
-	public void setX(double v) {
-		this.x = v;
-	}
-	public void setY(double v) {
-	this.y = v;
-	}
-	public void setR(double v) {
-	this.r = v;
-	}
+        return area1_hit || area2_hit || area3_hit;
+    }
 }
--- ОПИ/lab3/src/main/java/requestProcessors/ResultHandler.java
+++ ОПИ/lab3/src/main/java/requestProcessors/ResultHandler.java
@@ -16,48 +16,52 @@ import java.util.List;
 import java.util.Map;
 
 @Getter
-@Named @SessionScoped
+@Named
+@SessionScoped
 public class ResultHandler implements Serializable {
 
     private final ResultBean result = new ResultBean();
 
-	@Getter
+    @Getter
     private final List<ResultBean> resultList = new ArrayList<>();
 
     private final List<Double> availableR = List.of(1.0, 2.0, 3.0, 4.0, 5.0);
 
-	@Getter @Setter
-	private Double x;
-	@Getter @Setter
-	private Double y;
-	@Getter @Setter
-	private Double r;
+    @Getter
+    @Setter
+    private Double x;
+    @Getter
+    @Setter
+    private Double y;
+    @Getter
+    @Setter
+    private Double r;
 
-	@Getter
-	@Setter
-	private Double hiddenX;
-	@Getter
-	@Setter
-	private Double hiddenY;
+    @Getter
+    @Setter
+    private Double hiddenX;
+    @Getter
+    @Setter
+    private Double hiddenY;
 
-	@Getter
-	@Setter
-	private String autoFill;
+    @Getter
+    @Setter
+    private String autoFill;
 
-	@Inject
-	private DatabaseHandler databaseHandler;
-
-
-	public void processAutoFill() {
-		if ("true".equals(autoFill)) {
-			System.out.println("Автозаполнение с канваса");
-		} else {
-			System.out.println("Ручной ввод пользователя");
-		}
-	}
+    @Inject
+    private DatabaseHandler databaseHandler;
 
 
-	public void setResultParams() {
+    public void processAutoFill() {
+        if ("true".equals(autoFill)) {
+            System.out.println("Автозаполнение с канваса");
+        } else {
+            System.out.println("Ручной ввод пользователя");
+        }
+    }
+
+
+    public void setResultParams() {
         Map<String, String> requestParameters = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
         String error = requestParameters.get("error");
 
@@ -65,33 +69,32 @@ public class ResultHandler implements Serializable {
             MessageHandler.error(error);
             return;
         }
-		ResultBean result = new ResultBean();
-		System.out.println("x: "+x);
-		System.out.println("y: "+y);
-		System.out.println("hiddenX: "+hiddenX);
-		System.out.println("hiddenY: "+hiddenY);
+        ResultBean result = new ResultBean();
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("hiddenX: " + hiddenX);
+        System.out.println("hiddenY: " + hiddenY);
         try {
-			if (x != null) {
-				result.setX(x);
-			} else {
-				MessageHandler.error("Значение x не должно быть null.");
-				return;
-			}
+            if (x != null) {
+                result.setX(x);
+            } else {
+                MessageHandler.error("Значение x не должно быть null.");
+                return;
+            }
 
-			if (y != null) {
-				result.setY(y);
-			}
-			else {
-				result.setX(hiddenX);
-				result.setY(hiddenY);
-			}
-			result.setR(r);
+            if (y != null) {
+                result.setY(y);
+            } else {
+                result.setX(hiddenX);
+                result.setY(hiddenY);
+            }
+            result.setR(r);
 
-			result.setHit(false);
-			result.setTime("now");
-			result.setExecutionTime("0.01");
+            result.setHit(false);
+            result.setTime("now");
+            result.setExecutionTime("0.01");
         } catch (NumberFormatException | NullPointerException e) {
-			System.err.println(e);
+            System.err.println(e);
             MessageHandler.error("Некорректное значение.");
             return;
         }
@@ -103,12 +106,9 @@ public class ResultHandler implements Serializable {
         resultList.add(newResult);
     }
 
-	public void clean() {
-		resultList.clear();
-		databaseHandler.deleteAll();
-		MessageHandler.message(FacesMessage.SEVERITY_INFO, "Успех", "Все элементы успешно удалены");
-	}
-
-
-
+    public void clean() {
+        resultList.clear();
+        databaseHandler.deleteAll();
+        MessageHandler.message(FacesMessage.SEVERITY_INFO, "Успех", "Все элементы успешно удалены");
+    }
 }
